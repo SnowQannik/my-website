af3de1abcb8bdaa54628d5cb368907d7ef684b08
Uditanshu Khatri <udkhatri23@gmail.com>
2023-05-10 18:29:08 -0400

    changed some dependencies

--- src/components/Banner.js
+++ src/components/Banner.js
@@ -57,7 +57,7 @@ export const Banner = () => {
               <div className={isVisible ? "animate__animated animate__fadeIn" : ""}>
                 <span className="tagline">This is my Portfolio</span>
                 <h1>{`Hi! I'm Uditanshu`}  <span className="txt-rotate" dataPeriod="1000" data-rotate='[ "Software Developer", "Web Developer" ]'><span className="wrap">{text}</span></span></h1>
-                  <p>Welcome to my portfolio website! I'm Uditanshu Khatri, a Computer Science Masters student with experience teaching programming and database learning at Z King, and software development at Ledvance. Check out my projects below!</p>
+                  <p>Welcome to my portfolio website! I'm Uditanshu Khatri, a Computer Science Masters student with experience teaching programming and database learning at Z King, and software development at Ledvance. Check out some of my work below!</p>
                   {/* <button onClick={() => console.log('connect')}>Let’s Connect <ArrowRightCircle size={25} /></button> */}
                   <button>
                     <a className = 'resumeButton' href={resumePDF} download="Uditanshu Khatri Resume">
